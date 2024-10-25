95974d6e64808b905e245d59395782c1f9378450
Juan Martín López Frau <103914139+juanmaxx2@users.noreply.github.com>
2024-10-25 16:43:55 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Cambia la carpeta de salida de 'dist' a 'build'
+  },
 })
